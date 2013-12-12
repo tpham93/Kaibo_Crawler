295201279e8fb3c18bea3373403ac3f0c573df91
Kai Bornemann <ricu05@yahoo.de>
2013-12-12 17:33:44 +0100

    -ParticleEmitter.cs -Particle.cs

---
+++ Classes/Particle.cs
@@ -0,0 +1,73 @@
+﻿using SharpDX;
+using SharpDX.Toolkit;
+using SharpDX.Toolkit.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kaibo_Crawler
+{
+    public enum EParticleType {torchParticle, };
+
+    class Particle
+    {
+        public static Rectangle particleSource = new Rectangle(0,0,256,256);
+        public static Vector2 origin = new Vector2(128);
+
+
+        Color color;
+
+        Texture2D texture;
+
+        Vector2 position;
+        Vector2 direction;
+
+        float speed;
+        float rotation;
+        float scale;
+
+        public double lifeTime;
+
+        double maxLifetime;
+
+        float rotateDir = -1;
+
+        public Particle(Texture2D texture, Vector2 position, Vector2 direction, float speed, double lifeTime)
+        {
+            this.texture = texture;
+            this.position = position;
+            this.direction = direction;
+            this.speed = speed;
+            this.lifeTime = lifeTime;
+
+            this.scale = (float)(0.5 * Kaibo_Crawler.random.NextDouble());
+            this.rotation = (float)(Math.PI* Kaibo_Crawler.random.NextDouble());
+
+            if (Kaibo_Crawler.random.NextDouble() < 0.5)
+                rotateDir *= -1;
+
+            maxLifetime = lifeTime;
+        }
+
+        public void update(GameTime gameTime)
+        {
+            this.position = this.position + this.direction * this.speed;
+            lifeTime -= gameTime.ElapsedGameTime.TotalSeconds;
+
+            rotation += rotateDir * 0.005f;
+           
+
+            float t = (float)(lifeTime / maxLifetime);
+
+            color = Color.Lerp(Color.Red, Color.Yellow, t);
+        }
+
+        public void draw(SpriteBatch sb)
+        {
+            sb.Draw(texture, position, particleSource, color * (float)Math.Min(1.0d, lifeTime), rotation, origin, scale, SpriteEffects.None, 0);
+        }
+
+    }
+}
---
+++ Classes/ParticleEmitter.cs
@@ -0,0 +1,84 @@
+﻿using SharpDX;
+using SharpDX.Toolkit;
+using SharpDX.Toolkit.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kaibo_Crawler
+{
+    class ParticleEmitter
+    {
+        public Vector2 position;
+
+        int emitsPerSecond;
+
+        List<Particle> particles;
+
+        EParticleType type;
+
+
+        public ParticleEmitter(Vector2 position, EParticleType type, int emitsPerSecond)
+        {
+            this.type = type;
+            this.position = position;
+            this.emitsPerSecond = emitsPerSecond;
+
+            this.particles = new List<Particle>();
+        }
+
+        public void update(GameTime gameTime)
+        {
+            //TODO: currently its tick based, make it timebased:
+            emit(type, gameTime);
+            emit(type, gameTime);
+            emit(type, gameTime);
+
+
+            for (int i = 0; i < particles.Count; i++)
+            {
+                Particle p = particles.ElementAt(i);
+
+                p.update(gameTime);
+
+                if (p.lifeTime <= 0)
+                    particles.Remove(p);
+
+            }
+        }
+
+        public void draw(SpriteBatch sb)
+        {
+            foreach (Particle p in particles)
+                p.draw(sb);
+        }
+
+        private void emit(EParticleType type, GameTime gametime)
+        {
+            switch (type)
+            {
+                case EParticleType.torchParticle:
+
+
+                    Vector2 target = new Vector2(position.X, position.Y - 128);
+             
+                    Vector2 positionOffset = new Vector2(position.X + (float)Math.Sin(Math.PI*4*Kaibo_Crawler.random.NextDouble()) * 32.0f, position.Y);
+                    Vector2 direction = target - positionOffset;
+                    direction.Normalize();
+           
+
+                    float speed = 1;
+                    double lifeTime = 2.0d;
+
+
+                    particles.Add(new Particle(Kaibo_Crawler.particle, positionOffset, direction, speed, lifeTime));
+
+
+                    break;
+            }
+        }
+
+    }
+}
